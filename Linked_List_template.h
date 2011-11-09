d21f12503a4d6aca13e71728736c70bf011ddbd9
derwolfe <chriswwolfe@gmail.com>
2011-11-09 16:42:25 -0600

    began implementing

--- Linked_List_template.h
+++ Linked_List_template.h
@@ -8,38 +8,39 @@ using namespace std;
 /*
  * IMPLEMENT ME
  */
+template <class T>
 class Linked_List
 {
  private:
   /*
-   * IMPLEMENT ME
+   * Template class for node. Struct instead of class because
+   * it only holds data, nothing more. Class could be used, but 
+   * struct works.
    */
-    struct Node
-    {
-      Node* next;
-      /*
-       * IMPLEMENT ME
-       */
-    };
+  struct Node
+  {
+    T*    item;
+    Node* next;
+  };
 
+  /* 
+   * Length of the linked list
+   */
   int   size;
-
-  /*
-   * IMPLEMENT ME
-   */
+  Node* head;
+  Node* tail;
+  Node* find;
 
  public:
-  Linked_List();
+  Linked_List(); // do you need a copy constructor?
   ~Linked_List();
 
   int        get_size();
-  bool       is_empty();
+  bool       is_empty() const;
   void       print(std::ostream& os);
   void       remove(int index);
-
-  /*
-   * IMPLEMENT ME
-   */
+  T          append(const T& item);
+  T*         retrieve(int index) const; //retrieve at index, or item?
 };
 
 /*
@@ -61,6 +62,59 @@ class Linked_List
  * remove()
  * retrieve()
  */
+/* default constructor
+ */
+template <class T>
+Linked_List<T>::Linked_List ()
+{
+  size = 0;
+  head = NULL;
+  tail = NULL;
+}
 
+/* destructor calls remove at index
+ */
+template<class T>
+Linked_List::~Linked_List()
+{
+  while(!is_empty()) {
+    remove(1);
+  }
+}
+
+/* printing the items -- what method do I call? Do you require all
+ * users of the template to adhere to get_name??
+ */
+template<class T>
+void Linked_List::print(std::ostream& os)
+{
+  Node* cur = head;
+  while (cur != NULL) {
+    os << cur->item->get_title();
+  }
+}
+
+template<class T>
+bool Linked_List::is_empty() const
+{
+  if (size == 0) {
+    return true;
+  } // if size > 0 return false
+  return false;
+}
+
+/* 
+ * since this is a list we are going to append to the end
+ * that is the convention for the lab
+ */
+template<class T>
+bool Linked_List:append(const T* item) 
+{
+  if (is_empty()) {
+    Node* first = new Node<T>;
+    first->item = item;
+
+
+    
 #endif
 
