3262bfbfaebc83422c50b67230c4a565dccdeaf0
derwolfe <chriswwolfe@gmail.com>
2011-11-13 19:50:34 -0600

    test suite added, still having trouble with Node* find...

--- Linked_List_template.h
+++ Linked_List_template.h
@@ -33,13 +33,15 @@ class Linked_List
   Linked_List(); // do you need a copy constructor?
   ~Linked_List();
 
-  int        get_size() const;
-  bool       is_empty() const;
-  void       print(std::ostream& os);
-  void       remove(int index);
-  bool       append(T* item);
-  T*         retrieve(int index) const; //retrieve at index, or item?
-  
+  int       get_size() const;
+  bool      is_empty() const;
+  void      print(std::ostream& os);
+  bool      remove(int index);
+  bool      append(T* item);
+  Node*     find(int index) const; //retrieve at index, or item?
+  T         retrieve(int index) const;
+ // following used in testing 
+  void      tprint();
 };
 
 /*
@@ -73,26 +75,36 @@ Linked_List<T>::Linked_List ()
 
 /* destructor calls remove at index
  */
-//template<class T>
-//Linked_List<T>::~Linked_List()
-//{
-//  while(!is_empty()) {
-//    remove(1);
-//  }
-//}
-//
-///* printing the items -- what method do I call? Do you require all
-// * users of the template to adhere to get_name??
-// */
-//template<class T>
-//void Linked_List::print(std::ostream& os)
-//{
-//  Node* cur = head;
-//  while (cur != NULL) {
-//    os << cur->item->get_title();
-//  }
-//}
-//
+template<class T>
+Linked_List<T>::~Linked_List()
+{
+  while(!is_empty()) {
+    remove(1);
+  }
+}
+
+/* printing the items -- what method do I call? Do you require all
+ * users of the template to adhere to get_name??
+ */
+template<class T>
+void Linked_List<T>::print(std::ostream& os)
+{
+  Node* cur = head;
+  while (cur != NULL) {
+    os << cur->item << endl;
+    cur = cur->next;
+  }
+}
+
+template<class T>
+void Linked_List<T>::tprint()
+{
+  Node* cur = head;
+  while (cur != NULL) {
+    cout << *cur->item << endl;
+    cur = cur->next;
+  }
+}
 template<class T>
 bool Linked_List<T>::is_empty() const
 {
@@ -149,91 +161,104 @@ bool Linked_List<T>::append(T *item)
   }
 }
 
-/* how are you going to implement this? Your program is going to
+/* FIXME how are you going to implement this? Your program is going to
  * use nodes, nodes that always have some sort of pointer to more
- * data. 
+ * data.
+ *
+ * Wouldn't it be more useful to make an iterator, and implement the find
+ * function in the class using the list? Since there will be comparisons
+ * of varying types/methods. 
+ *
+ * For any items other than string/int this simply won't work.
  */
-//template<class T>
-//Node* Linked_List::find(string item) const
-//{
-//  Node* target = head;
-//  int inc = 1;
-//  while (inc <= (size + 1)) {
-//    if (inc == (size + 1)) {
-//      return NULL;
-//    } else if (target->T->get_name() == item ) { // is this wrong?
-//      return target;
-//    } else {
-//      target = target->next;
-//      inc++;
-//    }
-//  }
-//}
-//
-///* remove - think of scratching an item off of a list, any item
-// * can be removed, so you'll move some pointers around. Are we 
-// * removing by index or by track?
-// */
-//template<class T>
-//bool Linked_List::remove(int index)
-//{
-//  Node* target = head;
-//  Node* tmp = target;
-//  
-//  if (index < 0) {
-//    return false;
-//  }
-//  /* three conditions, head, middle area, tail
-//   */
-//  int inc = 0
-//  if (index == 0) { // covers head
-//    target = head;
-//    head = head->next;
-//    // what about tail?
-//    delete target;
-//    target = NULL;
-//    return true;
-//  } else if (get_size() < index) {
-//    // if index == 0, first test will catch this             
-//    while (inc < (index - 1)) { 
-//      //covers middle cases. It should select the node previous
-//      //to the node we want to delete, then set its next pointer
-//      //to the node following what we want to delete. 
-//      //
-//      //Also neither node is the head or the tail.
-//      target = target->next;
-//      inc++;
-//    }
-//    tmp = target->next; //target next is the node are deleting!
-//    target->next = target->next->next;
-//    delete tmp;
-//    target = NULL;
-//    tmp = NULL;
-//  } else { // delete the tail node
-//    target = tail;
-//    tmp = head;
-//    while (inc < (index - 1)) {
-//      tmp = tmp->next;
-//      inc++;
-//    } 
-//    tmp->next = NULL;
-//    delete target;
-//    target = NULL;
-//    tail = tmp;
-//    tmp = NULL; // may be unnecessary
-//  }
-//}
-//
-//template<class T>
-//T Linked_List::retrieve (string name) const
-//{
-//  Node* found = find(name);
-//  if (found == NULL){
-//    return NULL;
-//  } else {
-//    return found->item;//should return item or pointer to item.
-//  }
-//}
+
+template<class T>
+Node* Linked_List<T>::find(int index) const
+{
+  Node* target = head;
+  int inc = 1;
+  if (index == 1) {
+    return head; //already a pointer, ok
+  } else {
+    while (inc <= index) {
+      target = target->next;
+      target++;
+    } 
+    return target;
+  }
+}
+
+/* remove - think of scratching an item off of a list, any item
+ * can be removed, so you'll move some pointers around. Are we 
+ * removing by index or by track?
+ *
+ * What about size. You need to conform to the convention of the list
+ * beginning @ size 1.
+ */
+template<class T>
+bool Linked_List<T>::remove(int index)
+{
+//  index = index - 1; // conform to convention of pos beginning at one.
+  Node* target = head;
+  Node* prev = head;
+  int inc = 0;
+  // 3 conditions: either the head, the tail, or the a middle node is
+  // slated for deletion.
+  // 1. delete the head:
+  // WORKING
+  if (index == 1) {
+    head = head->next;
+    delete target;
+    target = NULL;
+    return true;
+  
+  } else if (index == size) {
+    while (inc <= size) { //again, finds previous node to tail
+      prev = target->next;
+      inc++;
+    }
+    delete tail;
+    tail = prev->next;
+    tail->next = NULL;
+    return true;
+    
+    /* WORKING, skipping over by one\
+     * 2. Delete a central node. I made a work around, I think I have lost
+     * track of something somewhere in this chunk.
+     */
+  
+  } else if ((index > 1) && (index < size)) {
+    inc = 1;
+    if (inc != (index - 1)) {
+      while (inc < (index - 1)) { 
+        // less will give the node preceding the target for deletion
+        prev = target->next;
+        inc++;
+      }
+    }
+    target = prev->next;
+    prev->next = prev->next->next;
+    delete target;
+    target = NULL;
+    prev = NULL;
+    return true;
+  } else { // for indexes that are out of bounds
+    return false;
+  }
+} 
+  
+template<class T>
+T Linked_List<T>::retrieve (int index) const
+{
+  // I am having trouble getting find to spit out a node, syntax issue. 
+  // this sets a global found variable to the pointer.
+  Node* target = find(index);
+  if (target == NULL){
+    return NULL;
+  } else {
+    return target->item;//should return item or pointer to item.
+  }
+}
 
 #endif
 
---
+++ tester.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include "Linked_List_template.h"
+
+int main ()
+{
+  Linked_List<int>* test = new Linked_List<int>;
+  int* a = new int(9);
+  int* b = new int(100);
+  int* c = new int(2100);
+  int* d = new int(892);
+
+  test->append(a);
+  test->append(b);
+  test->append(c);
+  test->append(d);
+  test->tprint();
+  test->remove(4);
+  cout << "after pos 4 removed" << endl;
+  test->tprint();
+  cout << "find item 2" << endl;
+}
+
